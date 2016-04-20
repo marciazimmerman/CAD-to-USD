659f9dd30ee2205f86a9da4754d26fc58bb5bda2
Marcia Zimmerman <marcia.zimmerman@gmail.com>
2016-04-20 13:21:31 -0700

    add quantities parameter

--- sample-cash-register.js
+++ sample-cash-register.js
@@ -1,3 +1,5 @@
+//simpler code that doesn't include quantities of the items
+
 var cashRegister = {
     total: 0,
     add: function(itemCost){
@@ -35,6 +37,33 @@ cashRegister.scan(eggs);
 cashRegister.scan(magazine);
 cashRegister.scan(magazine);
 cashRegister.scan(magazine);
+
+//Show the total bill
+console.log('Your bill is '+cashRegister.total);
+
+//now let's include quantities
+
+var cashRegister = {
+    total:0,
+    add: function(itemCost){
+        this.total += itemCost;
+    },
+    scan: function(item,quantity) {
+        this.quantity = quantity;
+        switch (item) {
+        case "eggs": this.add(0.98 * quantity); break;
+        case "milk": this.add(1.23 * quantity); break;
+        case "magazine": this.add(4.99 * quantity); break;
+        case "chocolate": this.add(0.45 * quantity); break;
+        }
+    }
+};
+
+// scan each item 4 times
+cashRegister.scan("eggs",4);
+cashRegister.scan("milk",4);
+cashRegister.scan("magazine",4);
+cashRegister.scan("chocolate",4);
 
 //Show the total bill
 console.log('Your bill is '+cashRegister.total);
